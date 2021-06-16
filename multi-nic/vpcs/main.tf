cbdfcb166b06a36f4f57da9b89942c1523e5ca5d
Greg Robinson <g.robinson@f5.com>
2021-06-15 22:32:54 -0400

    setting TCP monitor

--- multi-nic/vpcs/main.tf
+++ multi-nic/vpcs/main.tf
@@ -300,8 +300,8 @@ resource "aws_lb_target_group" "securityServicesTG" {
   port = 6081
   protocol = "GENEVE"
   health_check {
-    port = 443
-    protocol = "HTTPS"
+    port = 1
+    protocol = "TCP"
     healthy_threshold = 2
     unhealthy_threshold = 2
     timeout = 4
