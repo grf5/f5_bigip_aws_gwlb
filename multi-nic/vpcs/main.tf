6ad188916a8af6a5d2fc203a98cd7d69a45f476a
Greg Robinson <g.robinson@f5.com>
2021-06-28 14:13:17 -0400

    Removing NAT GATEWAY And mapping EIP directly to MGMT NIC

--- multi-nic/vpcs/main.tf
+++ multi-nic/vpcs/main.tf
@@ -137,42 +137,6 @@ resource "aws_internet_gateway" "securityServicesIGW" {
   }
 }
 
-resource "aws_eip" "securityServicesNGWEIPAZ1" {
-  vpc = true
-  tags = {
-    Name = "securityServicesNGWEIPAZ1"
-  }
-}
-
-resource "aws_eip" "securityServicesNGWEIPAZ2" {
-  vpc = true
-  tags = {
-    Name = "securityServicesNGWEIPAZ2"
-  }
-}
-
-resource "aws_nat_gateway" "securityServicesNGWAZ1" {
-  allocation_id =aws_eip.securityServicesNGWEIPAZ1.id
-  subnet_id = aws_subnet.securityServicesSubnetAZ1.id
-  tags = {
-    Name = "securityServicesNGWAZ1"
-  }
-  depends_on = [
-    aws_internet_gateway.securityServicesIGW
-  ]
-}
-
-resource "aws_nat_gateway" "securityServicesNGWAZ2" {
-  allocation_id =aws_eip.securityServicesNGWEIPAZ2.id
-  subnet_id = aws_subnet.securityServicesSubnetAZ2.id 
-  tags = {
-    Name = "securityServicesNGWAZ2"
-  }
-  depends_on = [
-    aws_internet_gateway.securityServicesIGW
-  ]
-}
-
 resource "aws_default_route_table" "securityServicesMainRT" {
   default_route_table_id = aws_vpc.securityServicesVPC.default_route_table_id
 
@@ -230,7 +194,7 @@ resource "aws_network_interface" "F5_BIGIP_AZ1ENI_MGMT" {
   }
 }
 
-resource "aws_eip" "F5_BIGIP_AZ1EIP" {
+resource "aws_eip" "F5_BIGIP_AZ1EIP_MGMT" {
   vpc = true
   network_interface = aws_network_interface.F5_BIGIP_AZ1ENI_MGMT.id
   associate_with_private_ip = aws_network_interface.F5_BIGIP_AZ1ENI_MGMT.private_ip
@@ -239,7 +203,20 @@ resource "aws_eip" "F5_BIGIP_AZ1EIP" {
     aws_internet_gateway.securityServicesIGW
   ]
   tags = {
-    Name = "F5_BIGIP_AZ1EIP"
+    Name = "F5_BIGIP_AZ1EIP_MGMT"
+  }
+}
+
+resource "aws_eip" "F5_BIGIP_AZ1EIP_DATA" {
+  vpc = true
+  network_interface = aws_network_interface.F5_BIGIP_AZ1ENI_DATA.id
+  associate_with_private_ip = aws_network_interface.F5_BIGIP_AZ1ENI_DATA.private_ip
+  # The IGW needs to exist before the EIP can be created
+  depends_on = [
+    aws_internet_gateway.securityServicesIGW
+  ]
+  tags = {
+    Name = "F5_BIGIP_AZ1EIP_DATA"
   }
 }
 
@@ -259,7 +236,7 @@ resource "aws_instance" "F5_BIGIP_AZ1" {
   }
   # Let's ensure an EIP is provisioned so licensing and bigip-runtime-init runs successfully
   depends_on = [
-    aws_eip.F5_BIGIP_AZ1EIP
+    aws_eip.F5_BIGIP_AZ1EIP_DATA
   ]
   tags = {
     Name = "${var.projectPrefix}-F5_BIGIP_AZ1-${random_id.buildSuffix.hex}"
@@ -285,7 +262,7 @@ resource "aws_network_interface" "F5_BIGIP_AZ2ENI_MGMT" {
   }
 }
 
-resource "aws_eip" "F5_BIGIP_AZ2EIP" {
+resource "aws_eip" "F5_BIGIP_AZ2EIP_MGMT" {
   vpc = true
   network_interface = aws_network_interface.F5_BIGIP_AZ2ENI_MGMT.id
   associate_with_private_ip = aws_network_interface.F5_BIGIP_AZ2ENI_MGMT.private_ip
@@ -294,10 +271,22 @@ resource "aws_eip" "F5_BIGIP_AZ2EIP" {
     aws_internet_gateway.securityServicesIGW
   ]
   tags = {
-    Name = "F5_BIGIP_AZ2EIP"
+    Name = "F5_BIGIP_AZ2EIP_MGMT"
   }
 }
 
+resource "aws_eip" "F5_BIGIP_AZ2EIP_DATA" {
+  vpc = true
+  network_interface = aws_network_interface.F5_BIGIP_AZ2ENI_DATA.id
+  associate_with_private_ip = aws_network_interface.F5_BIGIP_AZ2ENI_DATA.private_ip
+  # The IGW needs to exist before the EIP can be created
+  depends_on = [
+    aws_internet_gateway.securityServicesIGW
+  ]
+  tags = {
+    Name = "F5_BIGIP_AZ2EIP_DATA"
+  }
+}
 resource "aws_instance" "F5_BIGIP_AZ2" {
   ami               = data.aws_ami.f5BigIP_GWLB_AMI.id
   instance_type     = "c5.xlarge"
@@ -314,7 +303,7 @@ resource "aws_instance" "F5_BIGIP_AZ2" {
   }
   # Let's ensure an EIP is provisioned so licensing and bigip-runtime-init runs successfully
   depends_on = [
-    aws_eip.F5_BIGIP_AZ2EIP
+    aws_eip.F5_BIGIP_AZ2EIP_DATA
   ]
   tags = {
     Name = "${var.projectPrefix}-F5_BIGIP_AZ2-${random_id.buildSuffix.hex}"
